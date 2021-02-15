dc3a1f4e0c10d3719811ad61fda1df9371d5a784
Luis <luismanuelhn@gmail.com>
2021-02-15 09:08:43 -0600

    crud
    terminado

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBhe-HDFQuTkaoDffc835ViwsNVubdIIT4",
+    authDomain: "crud-3df1d.firebaseapp.com",
+    projectId: "crud-3df1d",
+    storageBucket: "crud-3df1d.appspot.com",
+    messagingSenderId: "357192407539",
+    appId: "1:357192407539:web:e6a90807ff35292b361a86"
+  };
+  export const firebaseApp=firebase.initializeApp(firebaseConfig) 
+ 
+
+ 
